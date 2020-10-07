7d2051965ed6fb41a02de90f472fbad421325a4b
Juanhernandez1 <2544692017@mail.utec.edu.sv>
2020-10-06 18:14:19 -0600

    📝 el calculo de %  de preguntas

---
+++ app/src/main/java/com/example/encuestadepoo/Finish.java
@@ -0,0 +1,13 @@
+package com.example.encuestadepoo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Finish extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish);
+    }
+}
